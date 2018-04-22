53c61de116dfd4558ff7921b9d80d6392c52bb4d
Matthew James Briggs <matthew.james.briggs@gmail.com>
2018-04-21 21:53:45 -0700

    fix errant comments in cmake build

--- Sourcecode/mxtest/file/PathRoot.h
+++ Sourcecode/mxtest/file/PathRoot.h
@@ -2,12 +2,12 @@
 // To override these paths, pass in these macro definitions to the compiler.
 #pragma once
 
-// The absolute path to the binary output directory.
+// The absolute path to the root of the repository.
 #ifndef MX_REPO_ROOT_PATH
 #define MX_REPO_ROOT_PATH "/Users/mjb/Documents/repos/mx"
 #endif
 
-// The absolute path to the root of the repository.
+// The absolute path to the binary output directory.
 #ifndef MX_BINARY_OUTPUT_PATH
 #define MX_BINARY_OUTPUT_PATH "/Users/mjb/Documents/repos/mx/cmake-build-debug"
 #endif
