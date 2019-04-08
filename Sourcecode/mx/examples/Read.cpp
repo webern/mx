4d8123c6fe54627bc7cfc9c886aa810492c54835
Matthew James Briggs <mjb@bitflip.software>
2019-04-07 21:00:13 -0700

    readme and examples #22 #26 (#28)
    * work on test options
    
    * work on examples
    
    * work on examples
    
    * build examples on circleci
    
    * run examples on circleci

---
+++ Sourcecode/mx/examples/Read.cpp
@@ -0,0 +1,102 @@
+#include <string>
+#include <iostream>
+#include <cstdint>
+#include <sstream>
+
+// TODO - create a proper include folder
+// normally we would want to include like this
+// #include "mx/api/DocumentManager.h"
+// however because our cmake system is a little bit
+// broken, we are importing from the mx/api directory
+// directly.
+
+#include "DocumentManager.h"
+#include "ScoreData.h"
+
+#define MX_IS_A_SUCCESS 0
+#define MX_IS_A_FAILURE 1
+
+constexpr const char* const xml = R"(
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE score-partwise PUBLIC
+    "-//Recordare//DTD MusicXML 3.1 Partwise//EN"
+    "http://www.musicxml.org/dtds/partwise.dtd">
+<score-partwise version="3.1">
+  <part-list>
+    <score-part id="P1">
+      <part-name>Music</part-name>
+    </score-part>
+  </part-list>
+  <part id="P1">
+    <measure number="1">
+      <attributes>
+        <divisions>1</divisions>
+        <key>
+          <fifths>0</fifths>
+        </key>
+        <time>
+          <beats>4</beats>
+          <beat-type>4</beat-type>
+        </time>
+        <clef>
+          <sign>G</sign>
+          <line>2</line>
+        </clef>
+      </attributes>
+      <note>
+        <pitch>
+          <step>C</step>
+          <octave>4</octave>
+        </pitch>
+        <duration>4</duration>
+        <type>whole</type>
+      </note>
+    </measure>
+  </part>
+</score-partwise>
+)";
+
+int main(int argc, const char * argv[])
+{
+    using namespace mx::api;
+
+    // create a reference to the singleton which holds documents in memory for us
+    auto& mgr = DocumentManager::getInstance();
+
+    // place the xml from above into a stream object
+    std::istringstream istr{ xml };
+
+    // ask the document manager to parse the xml into memory for us, returns a document ID.
+    const auto documentID = mgr.createFromStream( istr );
+
+    // get the structural representation of the score from the document manager
+    const auto score = mgr.getData( documentID );
+
+    // we need to explicitly destroy the document from memory
+    mgr.destroyDocument(documentID);
+
+    // make sure we have exactly one part
+    if( score.parts.size() != 1 )
+    {
+        return MX_IS_A_FAILURE;
+    }
+
+    // drill down into the data structure to retrieve the note
+    const auto& part = score.parts.at( 0 );
+    const auto& measure = part.measures.at( 0 );
+    const auto& staff = measure.staves.at( 0 );
+    const auto& voice = staff.voices.at( 0 );
+    const auto& note = voice.notes.at( 0 );
+
+    if( note.durationData.durationName != DurationName::whole )
+    {
+        return MX_IS_A_FAILURE;
+    }
+
+    if( note.pitchData.step != Step::c )
+    {
+        return MX_IS_A_FAILURE;
+    }
+
+    return MX_IS_A_SUCCESS;
+}
