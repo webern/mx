d3652f226b9cf9133aa73795d05b2807cf19ae16
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-07-05 21:28:11 -0700

    comment

--- CodeGen/src/model/enumeration.rs
+++ CodeGen/src/model/enumeration.rs
@@ -6,8 +6,8 @@ pub struct Enumeration {
     pub name: Symbol,
     pub members: Vec<Symbol>,
     pub documentation: String,
-    /// These are enums that are handled specially because they were part of an element that allowed
-    /// for an "other" field that could hold a string. If other_field.is_some(), it means that the
+    /// There are enums that are handled specially because they were part of an element that allowed
+    /// for an "other" field that could hold a string. If `other_field.is_some()`, it means that the
     /// enum behaves like a variant where some value can be held in a field that might be named, for
     /// example `other`. For an example of an 'enum' like this, see the `Dynamics` element in
     /// `musicxml.xsd`, which uses empty element choices for its enum values and an
