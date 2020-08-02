c05b920801e73978c86ea2b6b52f4830c748538a
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-08-02 12:16:38 -0700

    build

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ bld="${mx}/build"
 
 echo "mx=${mx}"
 echo "bld=${bld}"
+procs=$1
 
 mkdir -p "${bld}"
 cd "${bld}"
@@ -12,7 +13,7 @@ cmake "${mx}" \
   -DMX_BUILD_TESTS=on \
   -DMX_BUILD_CORE_TESTS=on \
   -DMX_BUILD_EXAMPLES=on
-make -j48
+make -C "${bld}/Makefile" -j"${procs:=48}"
 echo "Running MxRead"
 "${bld}/MxRead"
 echo "Running MxWrite"
