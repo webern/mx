3c80d6472ae32c6cab372ecb92756e44552c5bdd
Matt Briggs <brigmatt@amazon.com>
2020-08-02 19:04:05 +0000

    build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eo pipefail
+mx="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+bld="${mx}/build"
+
+echo "mx=${mx}"
+echo "bld=${bld}"
+
+mkdir -p "${bld}"
+cd "${bld}"
+cmake "${mx}" \
+  -DMX_BUILD_TESTS=on \
+  -DMX_BUILD_CORE_TESTS=on \
+  -DMX_BUILD_EXAMPLES=on
+make -j48
+echo "Running MxRead"
+"${bld}/MxRead"
+echo "Running MxWrite"
+"${bld}/MxWrite"
+echo "Running MxTest"
+"${bld}/MxTest"
