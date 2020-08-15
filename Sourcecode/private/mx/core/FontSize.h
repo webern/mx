bdfe70da2b4b1bf4d8178a327c5e79ab2b35f8b0
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-08-15 13:07:11 -0700

    core: improve decimals implementation

--- Sourcecode/private/mx/core/FontSize.h
+++ Sourcecode/private/mx/core/FontSize.h
@@ -7,6 +7,8 @@
 #include "mx/core/Decimals.h"
 #include "mx/core/Enums.h"
 
+#include <variant>
+
 namespace mx
 {
     namespace core
@@ -18,23 +20,17 @@ namespace mx
             explicit FontSize( const Decimal& value );
             explicit FontSize( const CssFontSize value );
             explicit FontSize( const std::string& value );
-            virtual ~FontSize();
-            FontSize( const FontSize& other );
-            FontSize( FontSize&& other );
-            FontSize& operator=( const FontSize& other );
-            FontSize& operator=( FontSize&& other );
-            
+
             bool getIsCssFontSize() const;
             bool getIsNumber() const;
             void setValue( const CssFontSize value );
             void setValue( const Decimal& value );
             CssFontSize getValueCssFontSize() const;
             Decimal getValueNumber() const;
-            void parse( const std::string& value );
+            bool parse( const std::string& value );
             
         private:
-            class impl;
-            std::unique_ptr<impl> myImpl;
+            std::variant<CssFontSize, Decimal> myValue;
         };
         
         std::string toString( const FontSize& value );
