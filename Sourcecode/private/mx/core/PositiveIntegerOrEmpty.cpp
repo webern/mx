abacdd46e0c78950f8fb2c2305f72b5c7f8d002f
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-08-16 15:17:49 -0700

    reset positive integer or empty

--- Sourcecode/private/mx/core/PositiveIntegerOrEmpty.cpp
+++ Sourcecode/private/mx/core/PositiveIntegerOrEmpty.cpp
@@ -2,91 +2,162 @@
 // Copyright (c) by Matthew James Briggs
 // Distributed under the MIT License
 
-// self
 #include "mx/core/PositiveIntegerOrEmpty.h"
-
-// std
+#include <string>
 #include <sstream>
-#include <type_traits>
+#include <memory>
 
 namespace mx
 {
     namespace core
     {
-        template<class> inline constexpr bool always_false_v = false;
-
+        
+        class PositiveIntegerOrEmpty::impl
+        {
+        public:
+            explicit impl()
+            :myPositiveInteger( 1 )
+            ,myIsEmpty( true )
+            {}
+            
+            explicit impl( const PositiveInteger& value )
+            :myPositiveInteger( value )
+            ,myIsEmpty( false )
+            {}
+            
+            explicit impl( const std::string& value )
+            :myPositiveInteger( 1 )
+            ,myIsEmpty( false )
+            {
+                parse( value );
+            }
+            
+            bool getIsEmpty() const
+            {
+                return myIsEmpty;
+            }
+            bool getIsNumber() const
+            {
+                return ! myIsEmpty;
+            }
+            void setValueEmpty()
+            {
+                myPositiveInteger = PositiveInteger( 1 );
+                myIsEmpty = true;;
+            }
+            void setValue( const PositiveInteger& value )
+            {
+                myPositiveInteger = PositiveInteger( value );
+                myIsEmpty = false;
+            }
+            PositiveInteger getValueNumber() const
+            {
+                return myPositiveInteger;
+            }
+            void parse( const std::string& value )
+            {
+                if ( value == "" )
+                {
+                    myPositiveInteger = PositiveInteger( 1 );
+                    myIsEmpty = true;
+                }
+                else
+                {
+                    /* if it contains only numeric
+                     characters it must be a number */
+                    myPositiveInteger.parse( value );
+                    myIsEmpty = false;
+                }
+            }
+        private:
+            PositiveInteger myPositiveInteger;
+            bool myIsEmpty;
+        };
+        
+        
         PositiveIntegerOrEmpty::PositiveIntegerOrEmpty()
-        : myValue{ PositiveInteger{} }
-        {
-
-        }
-
-        PositiveIntegerOrEmpty::PositiveIntegerOrEmpty( PositiveInteger value )
-        : myValue{ std::move( value ) }
-        {
-
-        }
+        :myImpl( new impl() )
+        {}
+        
+        PositiveIntegerOrEmpty::PositiveIntegerOrEmpty( const PositiveInteger& value )
+        :myImpl( new impl( value ) )
+        {}
         
         PositiveIntegerOrEmpty::PositiveIntegerOrEmpty( const std::string& value )
-        : PositiveIntegerOrEmpty{}
+        :myImpl( new impl( value ) )
+        {}
+        
+        PositiveIntegerOrEmpty::~PositiveIntegerOrEmpty() {}
+        
+        PositiveIntegerOrEmpty::PositiveIntegerOrEmpty( const PositiveIntegerOrEmpty& other )
+        :myImpl( new PositiveIntegerOrEmpty::impl( *other.myImpl ) )
+        {}
+        
+        PositiveIntegerOrEmpty::PositiveIntegerOrEmpty( PositiveIntegerOrEmpty&& other )
+        :myImpl( std::move( other.myImpl ) )
+        {}
+        
+        PositiveIntegerOrEmpty& PositiveIntegerOrEmpty::operator=( PositiveIntegerOrEmpty&& other )
         {
-            parse( value );
+            myImpl = std::move( other.myImpl );
+            return *this;
         }
-
-        bool PositiveIntegerOrEmpty::getIsPositiveInteger() const
+        
+        PositiveIntegerOrEmpty& PositiveIntegerOrEmpty::operator=( const PositiveIntegerOrEmpty& other )
         {
-            return myValue.index() == 0;
+            this->myImpl = std::unique_ptr<PositiveIntegerOrEmpty::impl>( new PositiveIntegerOrEmpty::impl( *other.myImpl ) );
+            return *this;
         }
-
-        void PositiveIntegerOrEmpty::setPositiveInteger( PositiveInteger value ) const
+        
+        bool PositiveIntegerOrEmpty::getIsEmpty() const
         {
-            myValue.emplace<PositiveInteger>( value );
+            return myImpl->getIsEmpty();
         }
-
-        PositiveInteger PositiveIntegerOrEmpty::getValuePositiveInteger() const
+        bool PositiveIntegerOrEmpty::getIsNumber() const
         {
-            auto result = PositiveInteger{};
-            std::visit([&](auto&& arg)
-            {
-                using T = std::decay_t<decltype(arg)>;
-                if constexpr( std::is_same_v<T, PositiveInteger> )
-                    result = arg;
-                else
-                    static_assert(always_false_v<T>, "non-exhaustive visitor!");
-            }, myValue);
-            return result;
+            return myImpl->getIsNumber();
         }
-
-        bool PositiveIntegerOrEmpty::parse( const std::string& value )
+        void PositiveIntegerOrEmpty::setValueEmpty()
         {
-            auto positiveInteger = PositiveInteger{};
-            if( positiveInteger.parse( value ) )
-            {
-                setValue( positiveInteger );
-                return true;
-            }
-            return false;
+            myImpl->setValueEmpty();
         }
-
+        void PositiveIntegerOrEmpty::setValue( const PositiveInteger& value )
+        {
+            myImpl->setValue( value );
+        }
+        PositiveInteger PositiveIntegerOrEmpty::getValueNumber() const
+        {
+            return myImpl->getValueNumber();
+        }
+        
+        void PositiveIntegerOrEmpty::parse( const std::string& value )
+        {
+            myImpl->parse( value );
+        }
+        
         std::string toString( const PositiveIntegerOrEmpty& value )
         {
             std::stringstream ss;
             toStream( ss, value );
             return ss.str();
         }
-
 		std::ostream& toStream( std::ostream& os, const PositiveIntegerOrEmpty& value )
         {
-            if( getIsPositiveInteger() )
+            if ( value.getIsNumber() )
             {
-                toStream( os, value.getValuePositiveInteger() );
+                toStream( os, value.getValueNumber() );
+            }
+            else
+            {
+                ; // os << "";
             }
             return os;
         }
-
 		std::ostream& operator<<( std::ostream& os, const PositiveIntegerOrEmpty& value )
         {
             return toStream( os, value );
         }
+        
+        
     }
 }
--- Sourcecode/private/mx/core/PositiveIntegerOrEmpty.h
+++ Sourcecode/private/mx/core/PositiveIntegerOrEmpty.h
@@ -4,31 +4,36 @@
 
 #pragma once
 
+#include <iostream>
 #include "mx/core/Integers.h"
-
-#include <ostream>
-#include <string>
-#include <variant>
+#include <memory>
 
 namespace mx
 {
     namespace core
     {
-        /// The positive-integer-or-empty values can be either a positive integer or an empty
-        /// string.
         class PositiveIntegerOrEmpty
         {
         public:
             explicit PositiveIntegerOrEmpty();
-            explicit PositiveIntegerOrEmpty( PositiveInteger value );
+            explicit PositiveIntegerOrEmpty( const PositiveInteger& value );
             explicit PositiveIntegerOrEmpty( const std::string& value );
-            bool getIsPositiveInteger() const;
-            void setPositiveInteger( PositiveInteger value );
-            PositiveInteger getValuePositiveInteger() const;
-            bool parse( const std::string& value );
+            virtual ~PositiveIntegerOrEmpty();
+            PositiveIntegerOrEmpty( const PositiveIntegerOrEmpty& other );
+            PositiveIntegerOrEmpty( PositiveIntegerOrEmpty&& other );
+            PositiveIntegerOrEmpty& operator=( const PositiveIntegerOrEmpty& other );
+            PositiveIntegerOrEmpty& operator=( PositiveIntegerOrEmpty&& other );
+            
+            bool getIsEmpty() const;
+            bool getIsNumber() const;
+            void setValueEmpty();
+            void setValue( const PositiveInteger& value );
+            PositiveInteger getValueNumber() const;
+            void parse( const std::string& value );
             
         private:
-            std::variant<PositiveInteger> myValue;
+            class impl;
+            std::unique_ptr<impl> myImpl;
         };
         
         std::string toString( const PositiveIntegerOrEmpty& value );
