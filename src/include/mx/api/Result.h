34e9c32ce982e5e01b014cbc0c26a2b1a5da399a
Matthew James Briggs <matthew.james.briggs@gmail.com>
2026-09-16 15:23:12 +0200

    Update src/include/mx/api/Result.h
    Signed-off-by: Matthew James Briggs <matthew.james.briggs@gmail.com>

--- src/include/mx/api/Result.h
+++ src/include/mx/api/Result.h
@@ -31,8 +31,6 @@ enum class ResultCode
     tooManyElements,
     invalidDocument,
     unsupportedVersion, // mirrored from the core parse boundary
-    // TODO: this badly swallows exception information. we need more variants
-    // or something like a site and message.
     internalError, // caught exception; nothing escapes    (api-level)
 };
 
